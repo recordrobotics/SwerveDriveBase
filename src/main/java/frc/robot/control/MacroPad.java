60b584f20b95aa7b6b596f919bd160c06690026f
Perry Cai <95951869+Person-26@users.noreply.github.com>
2025-01-13 21:23:26 -0500

    added limit switch

--- src/main/java/frc/robot/control/MacroPad.java
+++ src/main/java/frc/robot/control/MacroPad.java
@@ -4,44 +4,44 @@ import edu.wpi.first.wpilibj.GenericHID;
 
 public class MacroPad extends GenericHID {
 
-    public enum Button {
-        L1(1),
-        L2(2),
-        L3(3),
-        L4(4);
+  public enum Button {
+    L1(1),
+    L2(2),
+    L3(3),
+    L4(4);
 
-        private final int value;
+    private final int value;
 
-        Button(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
+    Button(int value) {
+      this.value = value;
     }
 
-    public MacroPad(int port) {
-        super(port);
+    public int getValue() {
+      return value;
     }
+  }
 
-    public boolean getButton(Button button) {
-        return getRawButton(button.getValue());
-    }
+  public MacroPad(int port) {
+    super(port);
+  }
 
-    public boolean getLevel1Button() {
-        return getButton(Button.L1);
-    }
+  public boolean getButton(Button button) {
+    return getRawButton(button.getValue());
+  }
 
-    public boolean getLevel2Button() {
-        return getButton(Button.L2);
-    }
+  public boolean getLevel1Button() {
+    return getButton(Button.L1);
+  }
 
-    public boolean getLevel3Button() {
-        return getButton(Button.L3);
-    }
+  public boolean getLevel2Button() {
+    return getButton(Button.L2);
+  }
 
-    public boolean getLevel4Button() {
-        return getButton(Button.L4);
-    }
+  public boolean getLevel3Button() {
+    return getButton(Button.L3);
+  }
+
+  public boolean getLevel4Button() {
+    return getButton(Button.L4);
+  }
 }
