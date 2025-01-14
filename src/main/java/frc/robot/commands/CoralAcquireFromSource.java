c9286dadf25206fbf2bbb06b041cb066a2bedc01
Isaac <barkerisaacd@gmail.com>
2025-01-13 20:29:15 -0500

    roname an add elevader

---
+++ src/main/java/frc/robot/commands/CoralAcquireFromSource.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.CoralAquisition.CoralAquisitionStates;
+import frc.robot.Constants.ElevatorHeight;
+
+public class CoralAcquireFromSource extends SequentialCommandGroup {
+  public CoralAcquireFromSource() {
+    addRequirements(RobotContainer.coralAquisition);
+
+    addCommands(
+        new InstantCommand(() -> RobotContainer.elevator.moveTo(ElevatorHeight.INTAKE)),
+        new WaitUntilCommand(() -> RobotContainer.elevator.atGoal()),
+        new InstantCommand(() -> RobotContainer.coralAquisition.toggle(CoralAquisitionStates.ACQUIRE)),
+        new WaitUntilCommand(() -> RobotContainer.coralShooter.hasCoral()),
+        new InstantCommand(() -> RobotContainer.coralAquisition.toggle(CoralAquisitionStates.OFF)));
+  }
+}
