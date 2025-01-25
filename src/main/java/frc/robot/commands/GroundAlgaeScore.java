535362ce5200f68cd37c4d8571cbc3a81326a6d5
Isaac <barkerisaacd@gmail.com>
2025-01-25 08:15:54 -0500

    add groundalgaescore

---
+++ src/main/java/frc/robot/commands/GroundAlgaeScore.java
@@ -0,0 +1,20 @@
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.GroundAlgae.GroundAlgaeStates;
+
+public class GroundAlgaeScore extends SequentialCommandGroup {
+  public GroundAlgaeScore() {
+    addRequirements(RobotContainer.groundAlgae);
+
+    addCommands(
+        new InstantCommand(() -> RobotContainer.groundAlgae.toggle(GroundAlgaeStates.OUT)),
+        new WaitUntilCommand(() -> RobotContainer.groundAlgae.hasAlgae()),
+        new InstantCommand(() -> RobotContainer.groundAlgae.toggle(GroundAlgaeStates.OFF))
+    );
+  }
+}
