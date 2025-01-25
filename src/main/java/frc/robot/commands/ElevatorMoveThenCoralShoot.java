a750095c5065d02bf6576cbe5f6a47640184050e
Isaac <barkerisaacd@gmail.com>
2025-01-25 13:28:04 -0500

    format the thing

--- src/main/java/frc/robot/commands/ElevatorMoveThenCoralShoot.java
+++ src/main/java/frc/robot/commands/ElevatorMoveThenCoralShoot.java
@@ -1,17 +1,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.RobotContainer;
 import frc.robot.Constants.ElevatorHeight;
+import frc.robot.RobotContainer;
 
 public class ElevatorMoveThenCoralShoot extends SequentialCommandGroup {
   public ElevatorMoveThenCoralShoot(ElevatorHeight targetHeight) {
     addRequirements(RobotContainer.coralShooter);
 
     addCommands(
-        new ElevatorMove(targetHeight),
-        new CoralShoot(),
-        new ElevatorMove(ElevatorHeight.INTAKE)
-    );
+        new ElevatorMove(targetHeight), new CoralShoot(), new ElevatorMove(ElevatorHeight.INTAKE));
   }
 }
