47121140ad7d42d6fb2a77236fef7c905ff5ecce
Compdog Inc. <compdog.inc@gmail.com>
2025-02-07 21:38:52 -0500

    Add development wpilib and fix led strip

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -7,6 +7,7 @@ import static edu.wpi.first.units.Units.Seconds;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.AddressableLED.ColorOrder;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -29,6 +30,7 @@ public class Lights extends SubsystemBase implements AutoCloseable {
   public Lights() {
     LEDs = new AddressableLED(RobotMap.Lights.LED_ID);
     buffer = new AddressableLEDBuffer(Constants.Lights.length);
+    LEDs.setColorOrder(ColorOrder.kRGB);
     LEDs.setLength(Constants.Lights.length);
     LEDs.start();
     setGlobalMode(LightMode.OFF);
