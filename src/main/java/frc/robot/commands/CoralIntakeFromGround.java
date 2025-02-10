c0e96f4e33efdd4b336b2ca8f9f2a3c755b8b296
Compdog Inc. <compdog.inc@gmail.com>
2025-02-10 03:15:58 -0500

    Reorder coral intake from ground

--- src/main/java/frc/robot/commands/CoralIntakeFromGround.java
+++ src/main/java/frc/robot/commands/CoralIntakeFromGround.java
@@ -16,17 +16,28 @@ public class CoralIntakeFromGround extends SequentialCommandGroup {
     addRequirements(RobotContainer.elevator);
 
     addCommands(
+        // start moving elevator to intake position
         new InstantCommand(() -> RobotContainer.elevator.moveTo(ElevatorHeight.INTAKE)),
+        // at the same time lower the arm
         new InstantCommand(() -> RobotContainer.coralIntake.toggleArm(IntakeArmStates.DOWN)),
         new WaitUntilCommand(() -> RobotContainer.coralIntake.armAtGoal()),
+        // start intaking
         new InstantCommand(() -> RobotContainer.coralIntake.toggle(CoralIntakeStates.INTAKE)),
+        // wait until coral is detected
         new WaitUntilCommand(() -> RobotContainer.coralIntake.hasCoral()),
+        // raise the arm
         new InstantCommand(() -> RobotContainer.coralIntake.toggleArm(IntakeArmStates.UP)),
-        new InstantCommand(() -> RobotContainer.coralIntake.toggle(CoralIntakeStates.OFF)),
         new WaitUntilCommand(() -> RobotContainer.coralIntake.armAtGoal()),
         new WaitUntilCommand(() -> RobotContainer.elevator.atGoal()),
+        // once both the arm and elevator are at goal, start elevator intake
         new InstantCommand(() -> RobotContainer.coralShooter.toggle(CoralShooterStates.INTAKE)),
+        // push coral out
+        new InstantCommand(() -> RobotContainer.coralIntake.toggle(CoralIntakeStates.REVERSE)),
+        // wait for elevator to have coral
         new WaitUntilCommand(() -> RobotContainer.coralShooter.hasCoral()),
+        // stop elevator intake
+        new InstantCommand(() -> RobotContainer.coralShooter.toggle(CoralShooterStates.OFF)),
+        // stop intake push out
         new InstantCommand(() -> RobotContainer.coralIntake.toggle(CoralIntakeStates.OFF)));
   }
 }
