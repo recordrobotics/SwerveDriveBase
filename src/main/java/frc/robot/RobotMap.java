05a38046db653d08ae18df3890fd3b41c06d8001
Isaac <barkerisaacd@gmail.com>
2025-02-23 13:55:07 -0500

    Remove TODO comments for endstop and motor IDs in RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,24 +4,24 @@ public final class RobotMap {
   public static final class Elevator {
     public static final int MOTOR_LEFT_ID = 10;
     public static final int MOTOR_RIGHT_ID = 11;
-    public static final int BOTTOM_ENDSTOP_ID = 4; // TODO get actual DIO port number
-    public static final int TOP_ENDSTOP_ID = 5; // TODO get actual DIO port number
+    public static final int BOTTOM_ENDSTOP_ID = 4;
+    public static final int TOP_ENDSTOP_ID = 5;
   }
 
   public static final class CoralShooter {
-    public static final int MOTOR_ID = 15; // TODO get actual CAN number
-    public static final int LIMIT_SWITCH_ID = 7; // TODO get actual DIO port number
+    public static final int MOTOR_ID = 15;
+    public static final int LIMIT_SWITCH_ID = 7;
   }
 
   public static final class AlgaeGrabber {
-    public static final int MOTOR_ID = 16; // TODO get actual CAN number
-    public static final int LIMIT_SWITCH_ID = 6; // TODO get actual DIO port number
+    public static final int MOTOR_ID = 16;
+    public static final int LIMIT_SWITCH_ID = 6;
   }
 
   public static final class CoralIntake {
     public static final int ARM_ID = 12;
-    public static final int WHEEL_ID = 14; // TODO get actual CAN number
-    public static final int LIMIT_SWITCH_ID = 10; // TODO get actual DIO port number
+    public static final int WHEEL_ID = 14;
+    public static final int LIMIT_SWITCH_ID = 10;
   }
 
   public static final class ElevatorArm {
@@ -29,10 +29,10 @@ public final class RobotMap {
   }
 
   public static final class Lights {
-    public static final int LED_ID = 0; // TODO get actual CAN number
+    public static final int LED_ID = 0;
   }
 
   public static final class Climber {
-    public static final int MOTOR_ID = 17; // TODO get actual CAN number
+    public static final int MOTOR_ID = 17;
   }
 }
