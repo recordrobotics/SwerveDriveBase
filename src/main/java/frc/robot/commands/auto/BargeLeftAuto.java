ece6db4454b66330adacd72439885ed326c2768c
Compdog Inc. <66779418+Compdog-inc@users.noreply.github.com>
2025-03-12 21:29:33 -0400

    Start barge left auto

---
+++ src/main/java/frc/robot/commands/auto/BargeLeftAuto.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.auto;
+
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.util.FileVersionException;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.Align;
+
+public class BargeLeftAuto extends SequentialCommandGroup {
+    
+    public BargeLeftAuto() throws FileVersionException, IOException, ParseException {
+        addCommands(
+            AutoBuilder.followPath(PathPlannerPath.fromPathFile("BargeLeftToL4")),
+            Align.create(0.01, 0.05, true).withTimeout(1.0)
+            );
+    }
+
+}
