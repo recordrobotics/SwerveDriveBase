138a458067a2ef9db85c1c04826dec0c3e88e960
Isaac <barkerisaacd@gmail.com>
2025-04-29 18:17:02 -0400

    make source align work

--- src/main/java/frc/robot/commands/hybrid/SourceAlign.java
+++ src/main/java/frc/robot/commands/hybrid/SourceAlign.java
@@ -1,9 +1,13 @@
 package frc.robot.commands.hybrid;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.Game.SourcePosition;
+import frc.robot.RobotContainer;
 
 public class SourceAlign {
   public static Command create(boolean useTranslation) {
-    return new AlignToPose(null, useTranslation); // TODO
+    return new AlignToPose(
+        SourcePosition.closestTo(RobotContainer.poseTracker.getEstimatedPosition()).getPose(),
+        useTranslation); // TODO
   }
 }
