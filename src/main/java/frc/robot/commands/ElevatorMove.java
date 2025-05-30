8b22a1ab7b9ff61fbfbf244a943f9b8034108863
Compdog Inc <compdog.inc@gmail.com>
2025-05-30 13:13:16 -0400

    Add elevatormove stall detection

--- src/main/java/frc/robot/commands/ElevatorMove.java
+++ src/main/java/frc/robot/commands/ElevatorMove.java
@@ -1,11 +1,15 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ElevatorHeight;
 import frc.robot.RobotContainer;
 
 public class ElevatorMove extends Command {
   private ElevatorHeight targetHeight;
+  private double lastMoveTime = 0;
+  private double initialElevatorHeight;
+  private double initialArmAngle;
 
   public ElevatorMove(ElevatorHeight targetHeight) {
     this.targetHeight = targetHeight;
@@ -15,6 +19,11 @@ public class ElevatorMove extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    lastMoveTime = Timer.getFPGATimestamp();
+
+    initialElevatorHeight = RobotContainer.elevator.getCurrentHeight();
+    initialArmAngle = RobotContainer.elevatorArm.getArmAngle();
+
     RobotContainer.elevator.moveTo(targetHeight);
     RobotContainer.elevatorArm.set(targetHeight.getArmAngle());
   }
@@ -24,6 +33,20 @@ public class ElevatorMove extends Command {
   public void execute() {
     RobotContainer.elevator.moveTo(targetHeight);
     RobotContainer.elevatorArm.set(targetHeight.getArmAngle());
+
+    if (Math.abs(RobotContainer.elevator.getCurrentVelocity()) >= 0.03
+        || RobotContainer.elevator.atGoal()) {
+      lastMoveTime = Timer.getFPGATimestamp();
+    } else {
+      // If the elevator is not moving, check if it has been stationary for too long
+      double currentTime = Timer.getFPGATimestamp();
+      if (currentTime - lastMoveTime > 0.1) { // elevator stalled
+        // Reset the elevator and arm to their initial positions
+        RobotContainer.elevator.set(initialElevatorHeight);
+        RobotContainer.elevatorArm.set(initialArmAngle);
+        cancel();
+      }
+    }
   }
 
   // Called once the command ends or is interrupted.
