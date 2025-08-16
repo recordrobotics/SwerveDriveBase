4dded91574a4ee27b1d4ce688d0c316d7a0cce97
Compdog Inc. <compdog.inc@gmail.com>
2025-08-16 06:49:47 -0400

    Add logging

--- src/main/java/frc/robot/utils/modifiers/AutoPathControlModifier.java
+++ src/main/java/frc/robot/utils/modifiers/AutoPathControlModifier.java
@@ -3,6 +3,7 @@ package frc.robot.utils.modifiers;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import org.littletonrobotics.junction.Logger;
 
 public class AutoPathControlModifier extends OneshotControlModifier {
 
@@ -15,6 +16,8 @@ public class AutoPathControlModifier extends OneshotControlModifier {
 
     @Override
     protected boolean perform(DrivetrainControl control) {
+        Logger.recordOutput("AutoPathControlModifier/Speeds", speeds);
+
         control.applyWeightedVelocity(
                 new Transform2d(
                         speeds.vxMetersPerSecond,
